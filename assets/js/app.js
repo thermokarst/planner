d16f7a999858dcebd0544d341e8984892f3965c5
Matthew Dillon <matthewrdillon@gmail.com>
2020-06-14 21:34:48 -0700

    NEW: styling

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,3 +29,17 @@ liveSocket.connect()
 // Call disableLatencySim() to disable:
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
+
+document.addEventListener('DOMContentLoaded', () => {
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    if ($navbarBurgers.length > 0) {
+      $navbarBurgers.forEach( el => {
+        el.addEventListener('click', () => {
+          const target = el.dataset.target;
+          const $target = document.getElementById(target);
+          el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+        });
+      });
+    }
+});
--- lib/planner_web/live/landing_live.ex
+++ lib/planner_web/live/landing_live.ex
@@ -10,74 +10,33 @@ defmodule PlannerWeb.LandingLive do
   def mount(_params, _session, socket) do
     socket =
       socket
-      # |> put_flash(:info, "hello world")
       |> assign(:new_task_changeset, Tasks.change_task(%Task{}))
-      |> assign(:tasks, Tasks.list_unfinished_tasks())
 
     {:ok, socket}
   end
 
   def render(assigns) do
     ~L"""
+    <div class="card">
     <%= f = form_for(@new_task_changeset, "#", [phx_submit: :save_new_task]) %>
-      <%= label f, :value, "New Task" %>
-      <%= text_input f, :value %>
       <%= error_tag f, :value %>
-
-      <%= submit "Create" %>
+      <%= text_input f, :value, placeholder: "add new task" %>
     </form>
-
-    <hr>
-
-    <table>
-      <thead>
-        <tr>
-          <th colspan="3">tasks</th>
-        </tr>
-      </thead>
-      <tbody>
-        <%= for task <- @tasks do %>
-          <tr>
-            <td><%= task.value %></td>
-            <td><button phx-click="delete_task" phx-value-task_id="<%= task.id %>">delete</button></td>
-            <td><button phx-click="finish_task" phx-value-task_id="<%= task.id %>">done</button></td>
-          </tr>
-        <% end %>
-      </tbody>
-    </table>
+    </div>
     """
   end
 
   def handle_event("save_new_task", %{"task" => task_params}, socket) do
     case Tasks.add_task(task_params) do
-      {:ok, task} ->
+      {:ok, _task} ->
         {:noreply,
          socket
-         |> put_flash(:info, "task created")
-         |> assign(:tasks, Tasks.list_unfinished_tasks())}
+         |> put_flash(:info, "task created")}
 
       {:error, %Ecto.Changeset{} = changeset} ->
         {:noreply,
          socket
          |> assign(new_task_changeset: changeset)}
     end
-  end
-
-  def handle_event("delete_task", %{"task_id" => task_id}, socket) do
-    Tasks.delete_task_by_id!(task_id)
-
-    {:noreply,
-     socket
-     |> put_flash(:info, "task deleted")
-     |> assign(:tasks, Tasks.list_unfinished_tasks())}
-  end
-
-  def handle_event("finish_task", %{"task_id" => task_id}, socket) do
-    Tasks.finish_task_by_id!(task_id)
-
-    {:noreply,
-     socket
-     |> put_flash(:info, "task completed")
-     |> assign(:tasks, Tasks.list_unfinished_tasks())}
   end
 end
