870e2389f6d59d87b6d105f411d85fc59c28c008
Matthew Dillon <matthewrdillon@gmail.com>
2020-06-25 21:16:07 -0700

    SQUASH

--- lib/planner_web/views/task_view.ex
+++ lib/planner_web/views/task_view.ex
@@ -1,9 +1,3 @@
 defmodule PlannerWeb.TaskView do
   use PlannerWeb, :view
-
-  # def md_to_html(md_text) do
-  #   md_text
-  #   |> Earmark.as_html!()
-  #   |> raw
-  # end
 end
--- lib/planner_web/views/util.ex
+++ lib/planner_web/views/util.ex
@@ -1,9 +1,10 @@
 defmodule PlannerWeb.Util do
   import Phoenix.HTML
+  alias Earmark.Options
 
   def md_to_html(md_text) do
     md_text
-    |> Earmark.as_html!()
+    |> Earmark.as_html!(%Options{smartypants: false})
     |> raw
   end
 end
