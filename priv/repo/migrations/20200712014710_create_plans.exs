063ad076b66114db9e939ede861cc94243b5af2d
Matthew Dillon <matthewrdillon@gmail.com>
2020-07-11 20:23:11 -0700

    squash

--- priv/repo/migrations/20200712014710_create_plans.exs
+++ priv/repo/migrations/20200712014710_create_plans.exs
@@ -2,8 +2,8 @@ defmodule Planner.Repo.Migrations.CreatePlans do
   use Ecto.Migration
 
   def change do
-    create table(:plans, primary_key: false)) do
-      add(:id, :binary, primary_key: true)
+    create table(:plans, primary_key: false) do
+      add(:id, :binary_id, primary_key: true)
       add(:name, :string)
       add(:start, :naive_datetime)
       add(:end, :naive_datetime)
@@ -14,8 +14,8 @@ defmodule Planner.Repo.Migrations.CreatePlans do
     end
 
     create table("tasks_in_plans", primary_key: false) do
-      add(:task_id, :binary, references(:tasks))
-      add(:plan_id, :binary, references(:plans))
+      add(:task_id, references(:tasks, type: :uuid))
+      add(:plan_id, references(:plans, type: :uuid))
       add(:sort, :integer, null: true)
 
       timestamps()
