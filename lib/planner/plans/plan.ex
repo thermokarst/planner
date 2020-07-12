003216980111b621b41e37923ef13767ab921c01
Matthew Dillon <matthewrdillon@gmail.com>
2020-07-11 19:12:09 -0700

    tweaks

--- lib/planner/plans/plan.ex
+++ lib/planner/plans/plan.ex
@@ -2,13 +2,15 @@ defmodule Planner.Plans.Plan do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
   schema "plans" do
-    field :description, :string
-    field :done, :boolean, default: false
-    field :end, :naive_datetime
-    field :id, :binary
-    field :name, :string
-    field :start, :naive_datetime
+    field(:description, :string)
+    field(:done, :boolean, default: false)
+    field(:end, :naive_datetime)
+    field(:name, :string)
+    field(:start, :naive_datetime)
+    many_to_many(:tasks, Planner.Tasks.Task, join_through: "tasks_in_plans")
 
     timestamps()
   end
--- priv/repo/migrations/20200712014710_create_plans.exs
+++ priv/repo/migrations/20200712014710_create_plans.exs
@@ -2,16 +2,23 @@ defmodule Planner.Repo.Migrations.CreatePlans do
   use Ecto.Migration
 
   def change do
-    create table(:plans) do
-      add :id, :binary
-      add :name, :string
-      add :start, :naive_datetime
-      add :end, :naive_datetime
-      add :done, :boolean, default: false, null: false
-      add :description, :string
+    create table(:plans, primary_key: false)) do
+      add(:id, :binary, primary_key: true)
+      add(:name, :string)
+      add(:start, :naive_datetime)
+      add(:end, :naive_datetime)
+      add(:done, :boolean, default: false, null: false)
+      add(:description, :string)
 
       timestamps()
     end
 
+    create table("tasks_in_plans", primary_key: false) do
+      add(:task_id, :binary, references(:tasks))
+      add(:plan_id, :binary, references(:plans))
+      add(:sort, :integer, null: true)
+
+      timestamps()
+    end
   end
 end
