005d343020cfd78b3ec1aba208b769ddcb824755
Matthew Dillon <matthewrdillon@gmail.com>
2022-01-10 16:04:30 -0700

    new: add support for optional link label

--- planner/links/forms.py
+++ planner/links/forms.py
@@ -8,13 +8,16 @@ from .models import Link
 class LinkForm(forms.ModelForm):
     class Meta:
         model = Link
-        fields = ['url', 'sets']
+        fields = ['url', 'label', 'sets']
 
+    label = forms.CharField(widget=forms.HiddenInput, required=False)
     url = forms.CharField(label='', widget=forms.TextInput(attrs={'autofocus': True}))
     sets = forms.CharField(widget=forms.HiddenInput, required=False)
 
 
 class LinkEditForm(LinkForm):
+    label = forms.CharField(label='label')
+
     sets = forms.ModelMultipleChoiceField(
         label='sets',
         queryset=Set.objects.filter(done=False).order_by('-updated_at'),
--- planner/links/models.py
+++ planner/links/models.py
@@ -9,6 +9,7 @@ _salts = settings.DJANGO_HASHID_SALTS
 class Link(models.Model):
     slug = HashidsField(real_field_name='id', salt=_salts['link'])
     url = models.URLField(verbose_name='URL')
+    label = models.CharField(max_length=500, blank=True, verbose_name='Label')
     sets = models.ManyToManyField('sets.Set', through='sets.SetLink', related_name='sets')
     done = models.BooleanField(default=False, verbose_name='Done?')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created At')
